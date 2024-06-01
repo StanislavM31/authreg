551244a90e250ad8fe45e818484ead13c1ab6494
Stanislav M <minkostas@gmail.com>
2024-06-01 23:58:40 +0300

    :registration

---
+++ form/login.php
@@ -0,0 +1,21 @@
+<!-- <!DOCTYPE html>
+<html>
+<head>
+    <title>Вход</title>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+</head>
+<body>
+    <h1>Вход13232</h1>
+    <form method="post" action="login_process.php">
+        <label for="login">Логин</label>
+        <input type="text" id="login" name="login" placeholder="Логин" required><br>
+
+        <label for="password">Пароль</label>
+        <input type="password" id="password" name="password" placeholder="Пароль" required><br>
+
+        <button type="submit">Войти</button>
+    </form>
+    <p>Еще не зарегистрированы? <a href="registration.php">Зарегистрироваться</a></p>
+</body>
+</html> -->
---
+++ form/registration/registration.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Регистрация</title>
+    <link rel="stylesheet" href="./style.css">
+    <meta charset="UTF-8">
+</head>
+<body>
+    <div class="container">
+        <h3>Регистрация</h3>
+        <form method="post" action="registration_process.php">
+            <label for="login">Логин</label>
+            <input type="text" id="login" name="login" placeholder="Логин" required><br>
+            
+            <label for="password">Пароль</label>
+            <input type="password" id="password" name="password" placeholder="Пароль" required><br>
+            
+            <label for="confirm_password">Подтверждение пароля</label>
+            <input type="password" id="confirm_password" name="confirm_password" placeholder="Подтверждение пароля" required><br>
+            
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" placeholder="Email" required><br>
+            
+            <button type="submit">Зарегистрироваться</button>
+        </form>
+        <p>Уже зарегистрированы? <a href="/">Войти</a></p>
+    </div>
+</body>
+</html>
